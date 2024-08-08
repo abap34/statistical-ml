1483474d5cdfe416b0da387e49fb9026263c9d8a
abap34 <abap0002@gmail.com>
2024-08-09 03:38:09 +0900

    refactor

--- ch04.jl
+++ ch04.jl
@@ -13,15 +13,15 @@ Random.seed!(34)
 
 x = rand(Normal(μ, sqrt(σ²)), N)
 
-μ_ml = sum(x) / N
-σ²_ml = sum((x .- μ_ml).^2) / N
+μ̂ = sum(x) / N
+σ̂² = sum((x .- μ̂).^2) / N
 
 println("μ_ml = ", μ_ml)
 println("σ²_ml = ", σ²_ml)
 
 histogram(x, bins=100, label="data", normed=true, alpha=0.5)
 plot!(x -> pdf(Normal(μ, sqrt(σ²)), x), label="true", linewidth=2)
-plot!(x -> pdf(Normal(μ_ml, sqrt(σ²_ml)), x), label="ML", linewidth=2)
+plot!(x -> pdf(Normal(μ̂, sqrt(σ̂²)), x), label="ML", linewidth=2)
 
 savefig("figure/ch04.png")
 
